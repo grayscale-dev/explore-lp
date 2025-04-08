1a5ebb4637ff61b38b58c8fc4da21ad5f8e13af8
zbrown001 <zbrown@nanolos.com>
2025-04-08 17:20:48 -0600

    create initial layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonTab, IonTabs, IonTabBar, IonTabButton, IonIcon } from '@ionic/angular/standalone';
+import { faHome, faCloud, faWater, faLayerGroup } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  imports: [IonApp, IonRouterOutlet],
+  imports: [IonApp, IonRouterOutlet,FontAwesomeModule, IonHeader, IonToolbar, IonTitle, IonContent, IonTab, IonTabs, IonTabBar, IonTabButton, IonIcon],
 })
 export class AppComponent {
-  constructor() {}
+  faHome = faHome
+  faCloud = faCloud
+  faWater = faWater
+  faLayerGroup = faLayerGroup
 }
