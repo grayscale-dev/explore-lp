5b815d8ef0ab7a1a7e18da740030406863646e86
zbrown001 <zbrown@nanolos.com>
2025-04-14 14:35:29 -0600

    update to new url

--- src/app/core/services/water-data.service.ts
+++ src/app/core/services/water-data.service.ts
@@ -6,13 +6,9 @@ import { WaterDataTransformer } from "src/app/models/water-data.model";
 })
 
 export class WaterDataService {
-    currentDate = new Date()
-    sixteenDaysAgo = new Date(this.currentDate).setDate(this.currentDate.getDate() - 16)
-    formattedDate = new Date(this.sixteenDaysAgo).toISOString().split('T')[0]
-
     data = resource({
         loader: async () => {
-            const url = `https://data.usbr.gov/rise/api/result?page=1&itemsPerPage=1000&locationId=393&parameterId=2,18,12,3&dateTime%5Bafter%5D=${this.formattedDate}`
+            const url = "https://stgexplorelp.blob.core.windows.net/daily-cache/usbr-water-data.json"
             const response = await fetch(url)
             if (!response.ok) {
                 throw new Error('Failed to fetch the Water Data')
